9eeef916b07ae0075c9a95bea902be00d93cefa0
501547952 <a501547952@n30501547952ml.eng.interplay3.prod>
2016-03-05 16:35:20 -0500

    I finished the project

--- story.rb
+++ story.rb
@@ -2,14 +2,15 @@ require 'pry'
 
 #write a method that accepts two parameters: title and rating
   #the method should print the story title and the rating out of ten
-
-
+def display_story(title, rating)
+  puts "your story is called #{title} and your rating is #{rating}"
+end
 #show the user a message asking them for a title
-
+puts "what is the title"
 #store the input in a variable called "user_title"
-
+user_title=gets.strip
 #show the user a message asking for their rating of the movie out of 100
-
+puts "what is the rating?"
 # store the input in a variable "user_rating"
-
+user_rating = gets.strip
 display_story(user_title, user_rating)
